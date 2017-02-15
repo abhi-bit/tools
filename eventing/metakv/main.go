4c5e6543fc9dbaf6f45096f22e30add5ee5f35fa
Abhishek Singh <abhishek@couchbase.com>
2017-02-15 11:53:06 -0800

    metakv observe

--- eventing/metakv/main.go
+++ eventing/metakv/main.go
@@ -62,25 +62,37 @@ func metakvRecursiveDel(dirpath string) error {
 	return err
 }
 
+var cancelCh chan struct{}
+
+func metaKVCallback(path string, value []byte, rev interface{}) error {
+	fmt.Printf("Noticed change in path: %s\n", path)
+	return nil
+}
+
 func main() {
 	_, err := cbauth.InternalRetryDefaultInit("http://127.0.0.1:9000", "Administrator", "asdasd")
 	if err != nil {
 		fmt.Printf("Failed to init abauth, err: %v\n", err)
 		return
 	}
+	cancelCh = make(chan struct{}, 1)
 
-	for i := 0; i < 10; i++ {
-		path := "/A/" + strconv.Itoa(i)
-		value := &TestBlob{A: i}
+	go metakv.RunObserveChildren("/", metaKVCallback, cancelCh)
 
-		err = metakvUpsert(path, value)
-		if err != nil {
-			fmt.Printf("Path: %s Upsert failed, err: %v\n", path, err)
-			continue
+	for i := 0; i < 2; i++ {
+		for i := 0; i < 2; i++ {
+			path := "/A/" + strconv.Itoa(i)
+			value := &TestBlob{A: i}
+
+			err = metakvUpsert(path, value)
+			if err != nil {
+				fmt.Printf("Path: %s Upsert failed, err: %v\n", path, err)
+				continue
+			}
+			var v interface{}
+			metakvFetch(path, &v)
+			fmt.Printf("Fetched from metakv path: %s value: %#v\n", path, v)
 		}
-		var v interface{}
-		metakvFetch(path, &v)
-		fmt.Printf("Fetched from metakv path: %s value: %#v\n", path, v)
 	}
 	metakvRecursiveDel("/A/")
 
