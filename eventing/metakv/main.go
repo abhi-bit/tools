c49c1c3007630702e9a902a484123bca52898af4
Abhishek Singh <abhishek@couchbase.com>
2017-02-15 13:36:30 -0800

    metakv listchild

--- eventing/metakv/main.go
+++ eventing/metakv/main.go
@@ -80,20 +80,29 @@ func main() {
 	go metakv.RunObserveChildren("/", metaKVCallback, cancelCh)
 
 	for i := 0; i < 2; i++ {
-		for i := 0; i < 2; i++ {
-			path := "/A/" + strconv.Itoa(i)
-			value := &TestBlob{A: i}
+		path := "/A/" + strconv.Itoa(i)
+		value := &TestBlob{A: i}
 
-			err = metakvUpsert(path, value)
-			if err != nil {
-				fmt.Printf("Path: %s Upsert failed, err: %v\n", path, err)
-				continue
-			}
-			var v interface{}
-			metakvFetch(path, &v)
-			fmt.Printf("Fetched from metakv path: %s value: %#v\n", path, v)
+		err = metakvUpsert(path, value)
+		if err != nil {
+			fmt.Printf("Path: %s Upsert failed, err: %v\n", path, err)
+			continue
+		}
+		var v interface{}
+		metakvFetch(path, &v)
+		fmt.Printf("Fetched from metakv path: %s value: %#v\n", path, v)
+	}
+
+	entries, err := metakv.ListAllChildren("/")
+	if err != nil {
+		fmt.Printf("Failed to list children, err: %v\n", err)
+	} else {
+		fmt.Printf("Children:\n")
+		for _, entry := range entries {
+			fmt.Printf("\tK: %s\n\tV: %s\n", entry.Path, string(entry.Value))
 		}
 	}
+
 	metakvRecursiveDel("/A/")
 
 	path := "/A/1"
