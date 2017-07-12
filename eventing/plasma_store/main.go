bf71864e45fb10d73bba65a5461db6004360c434
Abhishek Singh <singhabhishek.bit@gmail.com>
2017-07-12 16:04:27 +0530

    plasma iterator

--- eventing/plasma_store/main.go
+++ eventing/plasma_store/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	// "time"
 
 	"github.com/couchbase/nitro/plasma"
 )
@@ -11,16 +12,19 @@ func main() {
 	os.RemoveAll("teststore.data")
 	cfg := plasma.DefaultConfig()
 	cfg.File = "teststore.data"
+	cfg.AutoLSSCleaning = false
+	fmt.Printf("cfg dump: %#v\n", cfg)
 
 	s, err := plasma.New(cfg)
 	if err != nil {
 		panic(err)
 	}
-	defer s.Close()
 
 	w := s.NewWriter()
-	for i := 0; i < 10000; i++ {
-		w.InsertKV([]byte(fmt.Sprintf("key-%10d", i)), []byte(fmt.Sprintf("val-%10d", i)))
+	for j := 0; j < 5; j++ {
+		for i := 0; i < 5; i++ {
+			w.InsertKV([]byte(fmt.Sprintf("key-%10d", i)), []byte(fmt.Sprintf("val-%10d", i)))
+		}
 	}
 
 	k := []byte(fmt.Sprintf("key-%10d", 1000))
@@ -34,5 +38,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	s.PersistAll()
+
+	rdr := s.NewReader()
+	snapshot := s.NewSnapshot()
+
+	itr := rdr.NewSnapshotIterator(snapshot)
+	for itr.SeekFirst(); itr.Valid(); itr.Next() {
+		fmt.Println("1st key: ", string(itr.Key()), " value: ", string(itr.Value()))
+		fmt.Println("2nd key: ", string(itr.Key()), " value: ", string(itr.Value()))
+	}
+	itr.Close()
+
+	// time.Sleep(10 * time.Second)
 	fmt.Printf("Val: %v\n", string(v))
+	s.Close()
+
+	// time.Sleep(10 * time.Second)
 }
